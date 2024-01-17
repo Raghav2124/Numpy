738a0b7b252f848d41d5c9651e7e4bc07262a431
Raghav Aggarwal <raghav.aggarwal@testingxperts.loc>
2024-01-17 18:11:09 +0530

    Basic

---
+++ imp_num.py
@@ -0,0 +1,5 @@
+import numpy as np#alias method  to use a different name for numpy
+arr=np.array([1,2,3,4,5])#creating an array using array() function also pass tuples 
+print(arr)
+print(type(arr))#show the type of an array 
+print(np.__version__)#check the version of numpy we are using 
