0ccde0623a79bbf4b56551e0056e45ea907d0b56
Raghav Aggarwal <raghav.aggarwal@testingxperts.loc>
2024-01-18 09:59:58 +0530

    changes

--- Data_type.py
+++ Data_type.py
@@ -8,7 +8,7 @@ print(arr1)
 arr2 = np.array([1, 2, 3, 4], dtype='i4')
 print(arr2.dtype)
 arr3 = np.array([1.1, 2.1, 3.1])
-#alternate method 
+#alternate method to define the data type 
 newarr = arr3.astype('i')#The astype() function creates a copy of the array, and allows you to specify the data type as a parameter.
 print(newarr)
 print(newarr.dtype)
