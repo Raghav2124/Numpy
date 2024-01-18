394d7d19ef077b017b203e1a6d06224d3ea6843a
Raghav Aggarwal <raghav.aggarwal@testingxperts.loc>
2024-01-18 09:57:50 +0530

    data_type

---
+++ Data_type.py
@@ -0,0 +1,14 @@
+#dtype  returns the data type of the array
+import numpy as np
+arr=np.array([1,2,3,4])
+print(arr.dtype)
+arr1=np.array([2,3,4,5],dtype='S')
+print(arr1)
+
+arr2 = np.array([1, 2, 3, 4], dtype='i4')
+print(arr2.dtype)
+arr3 = np.array([1.1, 2.1, 3.1])
+#alternate method 
+newarr = arr3.astype('i')#The astype() function creates a copy of the array, and allows you to specify the data type as a parameter.
+print(newarr)
+print(newarr.dtype)
